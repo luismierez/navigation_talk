502d61629ecccc72c80f4f0b2ea7b27831e2101b
Luis Mierez <luismierez@gmail.com>
2018-08-26 09:06:51 -0700

    add navigation library

--- app/src/main/java/com/mierez/navigation/MainActivity.kt
+++ app/src/main/java/com/mierez/navigation/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.mierez.navigation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+    }
 }
